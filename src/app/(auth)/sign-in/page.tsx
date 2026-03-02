c228ef0d54dddb2adcfeaadf8e5ff0794aa77f76
Jannishar <mdjannishar654@gmail.com>
2026-03-02 14:14:50 +0530

    Sign In using NextAuth

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+// Note : jab parenthesis ke andar likha hota hai folder name toh wo route nhi collections hota hai 
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
--- src/middleware.ts
+++ src/middleware.ts
@@ -7,39 +7,39 @@ import {getToken }  from "next-auth/jwt"
 
 // It is main function 
 
-export async function middleware(req:NextRequest){
+export async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request });
+  const url = request.nextUrl;
 
-    const token = await getToken({req})  
-    const url =  req.nextUrl
+  // Redirect to dashboard if the user is already authenticated
+  // and trying to access sign-in, sign-up, or home page
+  if (
+    token &&
+    (url.pathname.startsWith('/sign-in') ||
+      url.pathname.startsWith('/sign-up') ||
+      url.pathname.startsWith('/verify') ||
+      url.pathname === '/')
+  ) {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
+  }
 
-    if(token && (
-        url.pathname.startsWith('/sign-in')||
-        url.pathname.startsWith('/sign-up')||
-        url.pathname.startsWith('/verify')||
-        url.pathname.startsWith('/')
-        
-    )
-    ){
-        return NextResponse.redirect(new URL("/dashboard", req.url)) 
+  if (!token && url.pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/sign-in', request.url));
+  }
 
-    }
-
-   return NextResponse.redirect(new URL("/home", req.url)) 
-     // Note : yaha humne middleware ko /api/auth ke sare routes pr apply kiya hai , iska matlab hai ki ye middleware /api/auth ke sare routes pr apply hoga , chahe wo /api/auth/login ho ya /api/auth/register ho ya koi aur route ho
+  return NextResponse.next();
 
 }
-
-
 // Config - Yeh btata hai ki middleware kin routes pr apply hoga , yaha humne /api/auth/* pr apply kiya hai , iska matlab hai ki ye middleware /api/auth ke sare routes pr apply hoga
 export const config = {
     matcher: [
        '/sign-in',
        '/sign-up',
        '/',
-       '/dashboard/: path*',
+       '/dashboard/:path*',
        '/verify/:path*'
 
 
     
-    ]  // Note : yaha :path* ka matlab hai ki ye middleware /api/auth ke sare routes pr apply hoga , chahe wo /api/auth/login ho ya /api/auth/register ho ya koi aur route ho
+    ],   // Note : yaha :path* ka matlab hai ki ye middleware /api/auth ke sare routes pr apply hoga , chahe wo /api/auth/login ho ya /api/auth/register ho ya koi aur route ho
 }
