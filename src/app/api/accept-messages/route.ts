53ecddfa2902411108ecdef8dfe1712af928dc8c
Jannishar <mdjannishar654@gmail.com>
2026-04-04 20:30:40 +0530

    accpet-messages route completed

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -77,3 +77,55 @@ export async function POST(request: Request) {
 
 }    
 
+export async function GET(request: Request) {
+
+     
+
+    await dbConnect();  
+
+    const session = await getServerSession(authOptions)
+    const user: User = session?.user as User // type assertion to treat session.user as a User object from next-auth, which should have the properties we need like email or id.
+
+    if(!session || !session.user){
+        return Response.json(
+            {
+                success: false,
+                message: "Unauthorized user "
+            },
+            {status: 401} 
+        )       
+    }  
+    
+    const userId = user._id; 
+
+  try{
+     const foundUser =await UserModel.findById(userId)
+    if(!foundUser){ 
+        return Response.json(
+            {
+                success: false,
+                message: "User not found"
+            },
+            {status: 404} 
+        )       
+    }
+
+    return Response.json(
+            {
+                success: true ,
+                isAcceptingMessages: foundUser.isAcceptingMessage   
+            },
+            {status: 200} 
+        )   
+    
+   } catch(error){
+    console.error("Failed to update user status to accept messages  ", error);
+    return Response.json(
+        {
+            success: false,
+            message: "Errors in getting message acceptance status"
+        },
+        {status: 500} 
+    )
+   }     
+}    
