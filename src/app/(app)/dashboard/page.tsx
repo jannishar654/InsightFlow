e47d6105f99020cbab6ff465afac3b38468a9fcc
Jannishar <mdjannishar654@gmail.com>
2026-04-22 16:11:49 +0530

    Dashboard - accept messages and fetch messages integrated

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -7,7 +7,7 @@ import axios, { AxiosError } from 'axios';
 import { ApiResponse } from '@/types/ApiResponse';
 
 import { useSession } from 'next-auth/react';
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect } from 'react'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form';
 import { toast } from "sonner"
@@ -32,7 +32,7 @@ const page = () => {
 
   const acceptMessages = watch('acceptMessages')
 
-  const fetchAcceptedMessages = useCallback(async () => {
+  const fetchAcceptMessage = useCallback(async () => {
     setIsSwitchLoading(true);
     try {
       const response = await axios.get('/api/accept-messages')
@@ -43,16 +43,79 @@ const page = () => {
     } catch(error){
       const axiosError = error as AxiosError<ApiResponse>
       toast.error(
-        axiosError.response?.data.message || "Failed to update message settings"
+        axiosError.response?.data.message || "Failed to fetch message settings"
       );
 
 
     } finally {
       setIsSwitchLoading(false)
-      
+
     }
   }, [setValue])
 
+  const fetchMessages = useCallback(async (refresh:boolean = false) =>{
+    setIsLoading(true)
+    setIsSwitchLoading(false)
+
+    try{
+       const response = await axios.get<ApiResponse>('/api/get-messages')
+       setMessages(response.data.messages || [])
+       if(refresh){
+        toast(
+        "Showing latest messages"
+       )
+
+       }
+    }
+    catch(error){
+      const axiosError = error as AxiosError<ApiResponse>
+      toast.error(
+        axiosError.response?.data.message || "Failed to update message settings"
+      );
+
+
+    } finally {
+      setIsLoading(false)
+      setIsSwitchLoading(false)
+
+    }
+  },[setIsLoading, setMessages])
+
+  useEffect(() => {
+    if(!session || !session.user) return 
+    fetchMessages()
+    fetchAcceptMessage()
+
+  }, [session, setValue, fetchAcceptMessage,fetchMessages])
+
+
+  // handle switch change 
+  const handleSwitchChange = async() => {
+    try{
+
+      const response = await axios.post<ApiResponse>('/api-accept-meesages', {
+        acceptMessages: !acceptMessages
+      })
+
+      setValue('acceptMessages',!acceptMessages)
+
+      toast(response.data.message);
+
+
+
+    }catch(error){
+      const axiosError = error as AxiosError<ApiResponse>
+      toast.error(
+        axiosError.response?.data.message || "Failed to fetch message settings"
+      );
+
+
+    }
+
+    if(!session || !session.user){
+      return <div> Please Login </div>
+    }
+  }
   return (
     <div>page</div>
   )
